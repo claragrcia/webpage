2f113c817d2e7ac77c603efd92cb90f6bc0fb624
claragrcia <31962637+claragrcia@users.noreply.github.com>
2018-01-29 13:50:56 -0500

    changes x100

--- js/main.js
+++ js/main.js
@@ -1,4 +1,52 @@
 $(document).ready(function(){
-    $("#hometext").fadeIn(3000);
-    });
+
+/// Resume section
+
+
+// first modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('resume1');
+var modalImg = document.getElementById("resumefirst");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// second modal
+
+var modal = document.getElementById('myModal2');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('resume2');
+var modalImg = document.getElementById("resumesecond");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+
+
 }); // do not delete
